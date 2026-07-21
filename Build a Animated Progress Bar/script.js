5628dbd4c52e1922828eca8922022ceb716bacc7
Bhanu Pratap <ppratapbhanu0@gmail.com>
2026-07-21 23:09:41 +0100

    Animation Bar

---
+++ Build a Animated Progress Bar/script.js
@@ -0,0 +1,14 @@
+const downloadBtn= document.querySelector(".download")
+const resetBtn= document.querySelector(".reset")
+const container= document.querySelector(".container")
+
+
+downloadBtn.addEventListener("click",() =>{
+    container.classList.add("change")
+}
+)
+
+resetBtn.addEventListener("click",() =>{
+    container.classList.remove("change")
+}
+)
